ac63dda67283518629901997a6a8d46ce80955c3
keisukefunatsu <keisukeyan2012@gmail.com>
2022-02-06 01:25:15 +0900

    customize way of layout use

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,25 @@
-import type { NextPage } from 'next'
-import Layout from '@/components/common/Layout'
+import type { InferGetStaticPropsType, NextPage } from 'next'
+import { Layout } from '@/components/common'
 import Link from "next/link"
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  return { props: {} }
+}
+const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout>
-      <Link href="/create_dao">
+      <Link href="/dao/create">
         <a className="m-10 py-2 px-4 border border-black-700 rounded">
           Create DAO
         </a>
       </Link>
       <Link href="/login_dao">
-      <a className="m-10 py-2 px-4 border border-black-700 rounded">
+        <a className="m-10 py-2 px-4 border border-black-700 rounded">
           Login DAO
         </a>
       </Link>
       <Link href="/marketplace">
-      <a className="m-10 py-2 px-4 border border-black-700 rounded">
+        <a className="m-10 py-2 px-4 border border-black-700 rounded">
           MarketPlace
         </a>
       </Link>
@@ -24,4 +27,5 @@ const Home: NextPage = () => {
   )
 }
 
+Home.Layout = Layout
 export default Home
