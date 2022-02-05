6f108d2bd3f832dfc2e2320df2b843b13d47a1a7
keisukefunatsu <keisukeyan2012@gmail.com>
2022-02-06 01:25:01 +0900

    add buttons to home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Footer from "@/components/common/Footer"
-import styles from '../styles/Home.module.scss'
 import Layout from '@/components/common/Layout'
+import Link from "next/link"
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1 className="text-5xl font-bold underline">
-        Hello, World
-      </h1>
+      <Link href="/create_dao">
+        <a className="m-10 py-2 px-4 border border-black-700 rounded">
+          Create DAO
+        </a>
+      </Link>
+      <Link href="/login_dao">
+      <a className="m-10 py-2 px-4 border border-black-700 rounded">
+          Login DAO
+        </a>
+      </Link>
+      <Link href="/marketplace">
+      <a className="m-10 py-2 px-4 border border-black-700 rounded">
+          MarketPlace
+        </a>
+      </Link>
     </Layout>
   )
 }
