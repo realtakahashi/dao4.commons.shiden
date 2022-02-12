8a397fca33ff36ed1682205fe17b8ec7e0dd4759
keisukefunatsu <keisukeyan2012@gmail.com>
2022-02-12 23:10:42 +0900

    merketplaceページ追加

---
+++ frontend/pages/dao/marketplace.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '@/components/common';
+
+const MarketPlace = () => {
+  return (
+    <>
+      This is MarketPlace
+    </>
+  )
+}
+
+MarketPlace.Layout = Layout
+export default MarketPlace
