1b9a5608757355e37955655b233c1348254819bb
keisukefunatsu <keisukeyan2012@gmail.com>
2022-02-13 01:07:07 +0900

    manage page修正

--- frontend/pages/dao/members/manage.tsx
+++ frontend/pages/dao/members/manage.tsx
@@ -5,7 +5,9 @@ const ManageMembers = () => {
     <>
       <div>
         <h2 className="text-xl">Add Member</h2>
-        <form className="w-full max-w-sm">
+        <form
+          className="w-full max-w-sm"
+        >
           <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
               <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
