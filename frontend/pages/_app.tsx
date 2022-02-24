fc909f0c5bd3e1b9f1bf4dfa08c0df9237ffef65
keisukefunatsu <keisukeyan2012@gmail.com>
2022-02-24 23:28:55 +0900

    ウォレット用providerの枠を作成

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { FC } from 'react'
+import { WalletProvider } from '@/contracts/context'
 
-const Noop: FC = ({children}) => <>{children}</>
 
-function DaoMaker({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
+const Noop: FC = ({ children }) => <>{children}</>
+
+function DaoMaker({ Component, pageProps }: AppProps & { Component: { Layout: FC } }) {
   const Layout = Component.Layout ?? Noop
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WalletProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </WalletProvider>
   )
 }
 
