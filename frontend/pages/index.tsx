904a598789c90b6dc676dee17021bfbf0bfb6638
keisukefunatsu <keisukeyan2012@gmail.com>
2022-03-05 23:53:11 +0900

    トップページのデザイン修正

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -41,48 +41,46 @@ const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
           })
         }
       </div>
-      <div className='mt-5 p-5'>
-        <h2 className="">List of Sub DAOs to which you belong</h2>
-        <div className="container my-12 mx-auto px-4 md:px-12">
-          <div className="flex flex-wrap -mx-1 lg:-mx-4">            
-            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">          
-            {typeof subDAOList !== "undefined" ?
-              subDAOList.map((dao) => {
-                return (
-                  <>                    
-                    <div
-                      className="bg-black my-2 border border-gray-700 hover:border-gray-500 max-w-sm rounded overflow-hidden shadow-lg"
-                      onMouseEnter={() => displayDAOData(dao.daoAddress)}
-                    >
-                      <div className="px-6 py-2">
-                        <div className="text-xl mb-2">{dao.daoName}</div>                  
-                      </div>
-                      <hr className='p-1 border-gray-700' />
-                      <div className="py-2 flex">
-                        <Link href={""}>
-                          <a
-                            className="inline-flex button-dao-default text-sm py-1 px-3"
-                          >
-                              Members
-                          </a>
-                        </Link>                        
-                        <Link href={""}>
-                          <a
-                            className="inline-flex button-dao-default text-sm py-1 px-3"
-                          >
-                            Proposals
-                          </a>
-                        </Link>                        
-                      </div>
+      <div className="container my-12 mx-auto px-4 md:px-12">
+        <h2 className="text-center">List of Sub DAOs to which you belong</h2>
+        <div className="flex flex-wrap justify-center mx-1 lg:-mx-4">
+
+          {typeof subDAOList !== "undefined" ?
+            subDAOList.map((dao) => {
+              return (
+                <>
+                  <div
+                    className="bg-black w-64 my-2 border border-gray-700 hover:border-gray-500 max-w-sm rounded overflow-hidden shadow-lg"
+                    onMouseEnter={() => displayDAOData(dao.daoAddress)}
+                  >
+                    <div className="px-6 py-2">
+                      <div className="text-xl mb-2">{dao.daoName}</div>
                     </div>
-                  </>
-                )
-              }) : ""
-            }
-            </div>
-          </div>
+                    <hr className='p-1 border-gray-700' />
+                    <div className="py-2 flex">
+                      <Link href={`/dao/${dao.daoAddress}/members`}>
+                        <a
+                          className="py-1 px-3 ml-7 inline-flex items-center button-dao-default text-sm"
+                        >
+                          Members
+                        </a>
+                      </Link>
+                      <Link href={`/dao/${dao.daoAddress}/proposals`}>
+                        <a
+                          className="py-1 px-3 ml-7 inline-flex items-center button-dao-default text-sm"
+                        >
+                          Proposals
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+                </>
+              )
+            }) : ""
+          }
         </div>
       </div>
+
 
 
       <div className='mt-5'>
