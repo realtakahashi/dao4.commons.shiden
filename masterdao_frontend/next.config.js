63b498c0609d9da6054b7073f22f2d3d8507e663
shin.takahashi <realtakahashi.0924@gmail.com>
2022-03-08 22:15:41 +0900

    環境変数を変更

--- masterdao_frontend/next.config.js
+++ masterdao_frontend/next.config.js
@@ -6,8 +6,5 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    MEMBER_NFT_CONTRACT_ADDRESS: process.env.MEMBER_NFT_CONTRACT_ADDRESS,
-    SUBDAO_CONTRACT_ADDRESS: process.env.SUBDAO_CONTRACT_ADDRESS,
-    MASTERDAO_CONTRACT_ADDRESS: process.env.MASTERDAO_CONTRACT_ADDRESS
   }
 }
