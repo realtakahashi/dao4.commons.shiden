4ebed2c8d0e342aa82637b19247e51302d8255d7
keisukefunatsu <keisukeyan2012@gmail.com>
2022-04-23 16:28:50 +0000

    scriptにmember追加のコードを追加

--- backend/scripts/deploy_test_data_set.ts
+++ backend/scripts/deploy_test_data_set.ts
@@ -28,35 +28,63 @@ async function main() {
   //set masterdao first member 
   await memberManager.addFirstMember(masterDAOContract.address, "Shin Takahashi", 0);
 
-  const MemberERC721ContractFactory = await ethers.getContractFactory("MemberERC721PresetMinterPauserAutoId");
-  const memberERC721Contract = await MemberERC721ContractFactory.deploy("TEST", "TEST", "test.com");
-  console.log("memberERC721 deployed to:", memberERC721Contract.address);
+
+
+  // set up members
   const subDaoOwner1 = await ethers.getSigner("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-  await memberERC721Contract
-    .connect(subDaoOwner1)
-    .original_mint(
-      subDaoOwner1.address, { value: ethers.utils.parseEther("2.0") }
-    );
-  console.log("memberERC721 connected to:", subDaoOwner1.address);
-  const subDAOContractFactory = await ethers.getContractFactory("SubDAO");
-  const subDaoContract = await subDAOContractFactory
-    .connect(subDaoOwner1)
-    .deploy(
-      "narusedai-2-36",
-      "test.com",
-      memberManager.address,
-      proposalManager.address,
-      memberERC721Contract.address
-    );
-  console.log("SubDAO deployed to:", subDaoContract.address);
+  const subDAOMemmbersAddresses = [
+    "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+    "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc",
+    "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+    "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",
+    "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc"
+  ]
+  const subDAOMemmbersSigners = subDAOMemmbersAddresses.map(async (memberAddress) => {
+    return await ethers.getSigner(memberAddress)
+  })
 
-  // add subDAO to masterDAO
-  await masterDAOContract.registerDAO(subDaoContract.address, "DAO test1", "https://github.com/realtakahashi/dao4.commons.shiden", "Description1");
-  console.log("subDAO: ", subDaoContract.address, "is connected to masterDAO: ", masterDAOContract.address);
+  // set up subDAO
+  const daoLoopCounter = 5
+  for (let i = 0; i < daoLoopCounter; i++) {
+    const MemberERC721ContractFactory = await ethers.getContractFactory("MemberERC721PresetMinterPauserAutoId");
+    const memberERC721Contract = await MemberERC721ContractFactory.deploy(`TEST${i}`, `TEST${i}`, `test${i}.com`);
+    console.log("memberERC721 deployed to:", memberERC721Contract.address);
+    await memberERC721Contract
+      .connect(subDaoOwner1)
+      .original_mint(
+        subDaoOwner1.address, { value: ethers.utils.parseEther("2.0") }
+      );
+    console.log("memberERC721 connected to:", subDaoOwner1.address);
+    const subDAOContractFactory = await ethers.getContractFactory("SubDAO");
+    const subDaoContract = await subDAOContractFactory
+      .connect(subDaoOwner1)
+      .deploy(
+        "narusedai-2-36",
+        "test.com",
+        memberManager.address,
+        proposalManager.address,
+        memberERC721Contract.address
+      );
+    console.log("SubDAO deployed to:", subDaoContract.address);
 
-  // add subdao owner as member manager
-  await memberManager.connect(subDaoOwner1).addFirstMember(subDaoContract.address, "Shin Takahashi", 1)
-  console.log("subDAO owner: ",subDaoOwner1.address, "is regstered to member manager: ", memberManager.address);
+    // add subDAO to masterDAO
+    await masterDAOContract.registerDAO(subDaoContract.address, `DAO test${i}`, "https://github.com/realtakahashi/dao4.commons.shiden", `Description ${i}`);
+    console.log("subDAO: ", subDaoContract.address, "is connected to masterDAO: ", masterDAOContract.address);
+
+    // add subdao owner as member manager
+    await memberManager.connect(subDaoOwner1).addFirstMember(subDaoContract.address, `Shin Takahashi ${i} `, 1)
+    console.log("subDAO owner: ", subDaoOwner1.address, "is regstered to member manager: ", memberManager.address);
+
+    subDAOMemmbersSigners.forEach(async (m) => {
+      const signer = await m
+      await memberERC721Contract
+        .connect(signer)
+        .original_mint(
+          signer.address, { value: ethers.utils.parseEther("2.0") }
+        );
+      console.log("memberERC721 connected to:", signer.address);
+    })
+  }
 }
 
 
