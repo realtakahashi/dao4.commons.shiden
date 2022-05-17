560a3929591bd152d0965cdff140c06c6e0cc250
keisukefunatsu <keisukeyan2012@gmail.com>
2022-05-17 14:15:36 +0000

    Revert "Revert "Frontend/refactoring""
    This reverts commit 60ba8c154701e16547cc436a2d4481939f1659cd.

---
+++ frontend/types/Token.ts
@@ -0,0 +1,37 @@
+export interface DaoErc20DeployFormData {
+  name: string
+  symbol: string
+  subDAOAddress: string
+  amount: number
+  pricewei: number
+}
+
+export interface DaoErc20 {
+  tokenAddress: string
+  tokenKind: number
+  name: string
+  symbol: string
+  totalBalance: number
+  onSale: boolean
+  salesAmount: number
+  price: number
+}
+
+export interface DaoErc721DeployFormData {
+  name: string
+  symbol: string
+  subDAOAddress: string
+  priceWei: number
+  tokenUri: string
+}
+
+export interface DaoErc721 {
+  tokenAddress: string
+  tokenKind: number
+  name: string
+  symbol: string
+  totalBalance: number
+  onSale: boolean
+  salesAmount: number
+  price: number
+}
